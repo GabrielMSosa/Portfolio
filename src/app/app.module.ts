3328a977132fae900c4bd57ff39dfd9746898cbb
GabrielMSosa <86896491+GabrielMSosa@users.noreply.github.com>
2022-01-03 23:38:34 -0300

    Primer comit agraga bootstrar/header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavheaderComponent } from './components/navheader/navheader.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavheaderComponent,
+    HeaderComponent
+  
   ],
   imports: [
     BrowserModule
